fb80668bada4ee8f0818c83a9ced20dcce4daf07
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2025-07-10 04:20:46 +0530

    Change C -_ c (capital to small.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 // const PORT = 5000;
 const PORT = process.env.PORT || 5000;
 
-const Contact = require('./models/Contact');
+const Contact = require('./models/contact');
 
 app.use(cors());
 app.use(express.json());
